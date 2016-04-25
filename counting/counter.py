3208dda6dfd67c47ebd49118f009b1c6fa048bd1
Lorena Pantano <lorena.pantano@gmail.com>
2016-04-25 21:17:54 +0000

    Keep only good pairs reads to avoid errors with htseq-count

--- counting/counter.py
+++ counting/counter.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser
 
 def get_counts(fn_in, gtf, out):
     if fn_in.endswith("bam") or fn_in.endswith("sam"):
-        cmd = "samtools view {fn_in} | htseq-count -i gene_name - {gtf} > {out}"
+        cmd = "samtools sort -n -O sam {fn_in} -o /dev/stdout | awk '$7==\"=\"' | htseq-count -i gene_name - {gtf} > {out}"
     do.run(cmd.format(**locals()), log_stdout=True)
 
 def get_position(gtf, out):
@@ -18,7 +18,7 @@ def get_position(gtf, out):
         with open(gtf) as in_h:
             for line in in_h:
                 cols = line.split("\t")
-                gene_id = [f.strip().split(" ")[1].replace("\"", "") for f in line.split("\t")[8].split(";") if f.strip().startswith("gene_id")]
+                gene_id = [f.strip().split(" ")[1].replace("\"", "") for f in line.split("\t")[8].split(";") if f.strip().startswith("gene_name")]
                 if cols[2] in ["gene", "transcript", "CDS"] and gene_id[0] not in seen:
                     print >>out_h, "%s\t%s\t%s\t%s" % (cols[0], gene_id[0], cols[3], cols[4])
                     seen.add(gene_id[0])
