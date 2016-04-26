e9941c6ef19aea6acadeaa95ba4578ad1bebe001
Lorena Pantano <lorena.pantano@gmail.com>
2016-04-26 19:45:06 +0000

    Change to NCBI annotation, handle SAM filtering.

--- counting/counter.py
+++ counting/counter.py
@@ -1,40 +1,104 @@
 import os
 import sys
 import logging
+import subprocess
+from contextlib import closing
+import xml.etree.ElementTree
+from collections import defaultdict
+import pandas as pd
+import HTSeq
 
 import do
+import mapper
 
 from argparse import ArgumentParser
 
+logger = logging.getLogger("Main")
+
+def _get_size(gtf):
+    gff_file = HTSeq.GFF_Reader( gtf, end_included=True )
+
+    transcripts= {}
+    data = defaultdict(dict)
+    gene = defaultdict()
+    for feature in gff_file:
+        if feature.type == "exon":
+            transcript_id = feature.attr['ID']
+  	    gene[transcript_id] = feature.attr['gene']
+            if transcript_id not in transcripts:
+                transcripts[ transcript_id ] = list()
+            transcripts[ transcript_id ].append( feature )
+      
+    for transcript_id in sorted( transcripts ):      
+        transcript_length = 0
+        for exon in transcripts[ transcript_id ]:
+            transcript_length += exon.iv.length + 1
+  	data[gene[transcript_id]].update({transcript_id: transcript_length})
+
+    for g in data.keys():
+	d = data[g]
+        for w in sorted(d, key=d.get, reverse=True):
+            data[g] = d[w]
+            break
+    return data
 
 def get_counts(fn_in, gtf, out):
     if fn_in.endswith("bam") or fn_in.endswith("sam"):
-        cmd = "samtools sort -n -O sam {fn_in} -o /dev/stdout | awk '$7==\"=\"' | htseq-count -i gene_name - {gtf} > {out}"
-    do.run(cmd.format(**locals()), log_stdout=True)
+        cmd = "samtools sort -n -O sam {fn_in} -o /dev/stdout | awk '$7==\"=\"' | htseq-count -s no -i gene - {gtf} > {out}"
+    elif fn_in.startswith("SRR"):
+        cmd = "sam-dump {fn_in} | samtools sort -n -O sam /dev/stdin -o /dev/stdout | awk '$7==\"=\"' | htseq-count -s no -i gene - {gtf} > {out}"
+	
+    logger.info(cmd.format(**locals()))
+    if not os.path.exists(out):
+        do.run(cmd.format(**locals()), log_stdout=True)
+    return out
 
-def get_position(gtf, out):
-    seen = set()
+def normalize(counts, gtf):
+    size = _get_size(gtf)
+    out = os.path.basename(counts).replace(".tsv", "_norm.tsv")
     with open(out, "w") as out_h:
-        with open(gtf) as in_h:
-            for line in in_h:
-                cols = line.split("\t")
-                gene_id = [f.strip().split(" ")[1].replace("\"", "") for f in line.split("\t")[8].split(";") if f.strip().startswith("gene_name")]
-                if cols[2] in ["gene", "transcript", "CDS"] and gene_id[0] not in seen:
-                    print >>out_h, "%s\t%s\t%s\t%s" % (cols[0], gene_id[0], cols[3], cols[4])
-                    seen.add(gene_id[0])
+	with open(counts) as in_h:
+    	    for line in in_h:
+                cols = line.strip().split("\t")
+                if cols[0] in size:
+	    	    rk = float(cols[1])/float(size[cols[0]])*1000
+                    print >>out_h, "%s\t%s" % (cols[0], rk)
+    return out
 
 def _get_gtf(version):
-    if version == "GRCh37":
-        url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz"
-    elif version == "GRCh38":
-        url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/gencode.v24.annotation.gtf.gz"
-
     file_out = "%s.gtf" % version
     gz_out = "%s.gz" % file_out
+    if os.path.exists(version):
+        return version
+    if os.path.exists(file_out):
+        return file_out
+    if version == "GRCh37":
+    	mapper.g37_map(file_out)
+        # url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz"
+    elif version == "GRCh38":
+    	mapper.g37_map(file_out)
+        # url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_24/gencode.v24.annotation.gtf.gz"
+    else:
+        raise ValueError("Species not supported %s" % version)
     cmd = "wget -O {gz_out} {url} && gunzip {gz_out} && sed -i 's/^chr//' {file_out}"
     if not os.path.exists(file_out):
         do.run(cmd.format(**locals()), log_stdout=True)
     return file_out
+
+def _check_samples(sra_id):
+    if not sra_id.startswith("SRR") or sra_id.endswith("sam"): # need better way to check if sam file or not
+	return None
+    cl = ("wget -q -O {sra_id} 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=sampleinfo&term={sra_id}'").format(**locals())
+    if not os.path.exists(sra_id):
+        do.run(cl)
+     
+    from xml.dom import minidom
+    xmldoc = minidom.parse(sra_id)
+    itemlist = xmldoc.getElementsByTagName('RUN')
+    for s in itemlist:
+	if s.attributes["accession"].value == sra_id:
+           return s.attributes['assembly'].value
+    raise ValueError("Sample not aligned.")
 
 def _set_log():
     logger = logging.getLogger()
@@ -43,18 +107,31 @@ def _set_log():
     logging_handler_out = logging.StreamHandler(sys.stdout)
     logging_handler_out.setLevel(logging.INFO)
     logger.addHandler(logging_handler_out)
-
+    return logger
 
 if __name__ == "__main__":
     description = ("Merge multiple files from the same sample to be compatible with bcbio BAM/FASTQ input files")
 
     parser = ArgumentParser(description="Merge fastq or bam files")
     parser.add_argument("--inp", required=True, help="SRA id or bam file")
-    parser.add_argument("--gtf", required=True, help="GTF file")
+    parser.add_argument("--gtf", required=False, help="GTF file")
     parser.add_argument("--out", required=True, help="output dir")
     args = parser.parse_args()
-    _set_log()
-    gtf = _get_gtf(args.gtf)
-    get_counts(args.inp, gtf,"%s.tsv" % args.out)
-    get_position(gtf, "%s_pos.tsv" % args.out)
+    logger = _set_log()
+    gtf = _check_samples(args.inp)
+    if not gtf:
+	gtf = args.gtf
+    if not gtf:
+        raise ValueError("Sample without aligment information or none GTF given.")
+    logger.info("Using this annotation %s" % gtf)
+    if not args.gtf:
+        gtf = _get_gtf(gtf)
+    elif not os.path.exists(args.gtf):
+	logger.info("Assuming version instead of gtf file")
+	gtf = _get_gtf(args.gtf)
+    logger.info("Using this file annotation %s" % gtf)
+    out_fn = get_counts(args.inp, gtf,"%s.tsv" % args.out)
+    out_fn = normalize(out_fn, gtf)
+    logger.info("Normalized file stored at %s" % out_fn)
+    # get_position(gtf, "%s_pos.tsv" % args.out)
 
